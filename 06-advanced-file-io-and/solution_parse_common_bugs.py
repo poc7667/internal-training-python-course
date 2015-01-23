eb353e3299ea46e4dc7c3d8e0c9fa5b271ef0cd4
poc7667 <poc7667@gmail.com>
2015-01-23 14:30:19 +0800

    hw5_sol: read line and find the target line

--- 06-advanced-file-io-and/solution_parse_common_bugs.py
+++ 06-advanced-file-io-and/solution_parse_common_bugs.py
@@ -1,2 +1,10 @@
 target_line = None
 fixed_bugs = []
+
+# open file and read lines
+with open("./history.txt", 'r') as f :
+    for line in f :
+        if "Verify major bug fixed" in line:
+            target_line = line
+            break
+print(target_line)
