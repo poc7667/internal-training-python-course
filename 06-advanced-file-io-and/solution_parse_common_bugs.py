42d6edef6b76d15f347a763abee0794f33cc1551
poc7667 <poc7667@gmail.com>
2015-01-23 14:32:09 +0800

    hw5: convert bugs into a list

--- 06-advanced-file-io-and/solution_parse_common_bugs.py
+++ 06-advanced-file-io-and/solution_parse_common_bugs.py
@@ -16,3 +16,12 @@ print target_line
 print target_line.split("<")[-1].split(">")[0]
 trimmed_bugs_info = target_line.split("<")[-1].split(">")[0].split(",")
 # print trimmed_bugs_info
+
+
+# stage 3 : convert bugs into a list
+for bug in trimmed_bugs_info:
+    if "-" in bug:
+        bug_range = bug.split("-")
+        fixed_bugs += list(range(int(bug_range[0]), int(bug_range[1])+1))
+    else:
+        fixed_bugs.append(int(bug))
